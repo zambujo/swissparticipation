90e386f92218521497163af8a965be345057f1e3
zambujo <zambuj@gmail.com>
2022-02-16 23:29:19 +0100

    ♻️  png -> svg

--- Makefile
+++ Makefile
@@ -1,18 +1,5 @@
 all: site
 
-update:
-	cd R;\
-	Rscript render-data-gathering.R;
-
-seri:
-	cd R;\
-	Rscript render-seri-figures.R;
-
-index:
-	cd R;\
-	Rscript render-swiss-participation.R;
-
 site:
 	cd R;\
 	Rscript render-site.R;
-
--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,19 @@
 int <- function(x) as.integer(x)
 chr <- function(x) as.character(x)
 roman <- function(x) as.roman(x)
+
+
+own_theme <- function() {
+    ret <- (theme_minimal() +
+        theme(
+            plot.title.position = "plot",
+            legend.position = "top",
+            legend.title = element_blank(),
+            text = element_text(
+                size = 15,
+                family = "Gill Sans",
+                color = "gray15"
+            )
+        ))
+    ret
+}
